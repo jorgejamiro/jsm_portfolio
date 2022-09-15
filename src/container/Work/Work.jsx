7d29c8cc9ef380f463b5a0f401a9f473697b242c
unknown <jorgejaykay@hotmail.com>
2022-09-16 00:43:36 +0200

    fixing Work Cards: showing links when small devices

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -54,7 +54,7 @@ const Work = () => {
               <div
                 key={index}
                 onClick={() => handleWorkFilter(item)}
-                className={`app__work-filer-item app__flex p-text 
+                className={`app__work-filter-item app__flex p-text 
                             ${activeFilter === item ? 'item-active' : ''}`}
               >
                 {item}
