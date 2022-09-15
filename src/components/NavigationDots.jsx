094e8edfcea4a24a82d9f51254131ff1cf8f0ecd
unknown <jorgejaykay@hotmail.com>
2022-09-15 13:31:41 +0200

    initial upload

---
+++ src/components/NavigationDots.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const NavigationDots = ({ active }) => {
+  return (
+    <div className='app__navigation'>
+    {
+        ['home', 'about', 'work', 'skills', 'testimonial', 'contact']
+            .map((item, index) => (
+                <a 
+                    href={`#${item}`}
+                    key={item + index}
+                    className='app__navigation-dot'
+                    onClick={() => setToggle(false)}
+                    style={active === item ? { backgroundColor: '#313BAC'} : {}}
+                />
+            ))
+    }
+
+
+    </div>
+  );
+}
+
+export default NavigationDots;
